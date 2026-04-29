ea4547fdd62ea61cfd90d6c0dbd26e98e14b078b
ewenparis48-art <ewen.paris.48@gmail.com>
2026-04-29 23:40:52 +0200

    token env

--- bot.py
+++ bot.py
@@ -1,7 +1,8 @@
 from telegram import Update
 from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
 
-TOKEN = "8606061066:AAFsy5tny55nbzrfCxsUm2HVjLcFIk4aDDo"
+import os
+TOKEN = os.environ.get("8606061066:AAFsy5tny55nbzrfCxsUm2HVjLcFIk4aDDo")
 
 async def accueillir(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for membre in update.message.new_chat_members:
