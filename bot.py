07653b77259bbb8adad12773bd81927e4b15827b
ewenparis48-art <ewen.paris.48@gmail.com>
2026-04-30 12:26:21 +0200

    nouveau message

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = os.environ.get("TOKEN")
 async def accueillir(update: Update, context: ContextTypes.DEFAULT_TYPE):
     for membre in update.message.new_chat_members:
         prenom = membre.first_name
-        await update.message.reply_text(f"👋 Bienvenue {prenom} ! Heureux de t'avoir parmi nous.")
+       await update.message.reply_text(f"Bonjour {prenom} c'est un plaisir de t'avoir parmi nous ! 😊\n\nBienvenue dans cette communauté à but d'entraide et de recherche, je t'invite désormais à consulter les différentes rubriques.\nSi tu as quelconques questions n'hésite pas ! 🔥")
 
 app = ApplicationBuilder().token(TOKEN).build()
 app.add_handler(MessageHandler(filters.StatusUpdate.NEW_CHAT_MEMBERS, accueillir))
